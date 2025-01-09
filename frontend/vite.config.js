87de4f8ef5acc644ce222fc50ac3cd924942f694
Kalinka5 <dania003d@gmail.com>
2025-01-09 13:29:59 +0200

    Updated css

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: "/frontend/", // Adjust the base URL for GitHub Pages
+  plugins: [react()], // Adjust the base URL for GitHub Pages
+  base: "/web-chatbot/", // Set the base to your repo name
 });
