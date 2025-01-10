2b35b8bc801d32c682803ac34df6df4155e271fd
Kalinka5 <dania003d@gmail.com>
2025-01-10 16:04:00 +0200

    Updates

--- embed-chatbot.js
+++ embed-chatbot.js
@@ -1,29 +1,16 @@
-(async function () {
-  // Fetch the manifest file
-  const response = await fetch(
-    "https://kalinka5.github.io/web-chatbot/.vite/manifest.json"
-  );
-  const manifest = await response.json();
-
-  // Get the hashed file names from the manifest
-  const cssFile = manifest["index.html"].file;
-  const jsFile = manifest["index.html"].css;
-
-  // Dynamically load the CSS file
+(function () {
   const link = document.createElement("link");
   link.rel = "stylesheet";
-  link.href = `https://kalinka5.github.io/web-chatbot/${cssFile}`;
+  link.href = "https://kalinka5.github.io/web-chatbot/chatbot2.css";
   link.crossOrigin = "anonymous";
   document.head.appendChild(link);
 
-  // Dynamically load the JS file
   const script = document.createElement("script");
   script.type = "module";
-  script.src = `https://kalinka5.github.io/web-chatbot/${jsFile}`;
+  script.src = "https://kalinka5.github.io/web-chatbot/assets/chatbot.js";
   script.crossOrigin = "anonymous";
   document.head.appendChild(script);
 
-  // Initialize the chatbot once the script is loaded
   script.onload = () => {
     const chatbot = document.createElement("r2w-chatbot");
     document.body.appendChild(chatbot);
